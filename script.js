d1e927895ef84bb59644c35314a566fde8e51d51
Kira Iovenko <103902421+kira-iovenko@users.noreply.github.com>
2024-08-05 19:54:02 +0000

    Declare and initialize a variable to store each outcome and set each one equal to 0, declare a variable to store the number of questions answered and set it equal to 0, access each button element using its id name and the getElementById() method, create six variables to store the value of each answer button

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 /*Add your JavaScript here*/
+
+/*variable to store each outcome*/
+var eliScore = 0;
+var joScore = 0;
+
+/*variable to store the number of questions answered*/
+var numQuestion = 0;
+
+/*variables to store the buttons*/
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
